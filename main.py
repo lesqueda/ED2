5304c947f996e30695a6e7e2b5fae31a1e96a48d
danigc02 <75408710+mrmaniak99@users.noreply.github.com>
2021-02-23 21:17:00 -0500

    actualizacion de inorder

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ class Node():
 
     def insert(self):
         pass
+    #carlos gonzalez y eduardo paris
+    def inorder(self, value):
+    if a==None:
+        return None
+    else:
+        self.inorder(a.left)
+        print(a.dato)
+        self.inorder(a.right)
