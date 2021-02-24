4902782dc6df4f9c391176ef27cf8bdcd07b823e
Maikel Franco <mikefranco19@hotmail.com>
2021-02-24 17:41:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ class Node():
         self.value=value
         self.left=None
         self.right=None
+        
+    class arbol():
+    def __init__(self):
+        self.root = None
 
       def insert(self, value):
         if self.value:
@@ -29,3 +33,53 @@ class Node():
         print(a.dato)
         self.inorder(a.right)
 
+tree = arbol()
+
+#Muestra las opciones de recorrido y tratamiento del árbol
+while True:
+    os.system("cls")
+    print("Arbol Binario. Opciones:")
+    opt = input("\n1.-Insertar nodo \n2.-Inorden \n3.-Preorden \n4.-Postorden \n5.-Buscar \n6.-Salir \n\nPor favor elija una opcion -> ")
+
+    if opt == '1':
+        nodo = input("\nIngrese el nodo -> ")
+        if nodo.isdigit():
+            nodo = int(nodo)
+            tree.root = tree.insert(tree.root, nodo)
+        else:
+            print("\nIngrese solo digitos...")
+    elif opt == '2':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.inorder(tree.root)
+    elif opt == '3':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.preorder(tree.root)
+    elif opt == '4':
+        if tree.root == None:
+            print("Vacio")
+        else:
+            tree.postorder(tree.root)
+    elif opt == '5':
+        nodo = input("\nIngrese el nodo a buscar -> ")
+        if nodo.isdigit():
+            nodo = int(nodo)
+            if tree.buscar(nodo, tree.root) == None:
+                print("\nNodo no encontrado...")
+            else:
+                print("\nNodo encontrado -> ",tree.buscar(nodo, tree.root), " si existe...")
+        else:
+            print("\nIngrese solo digitos...")        
+    elif opt == '6':
+        print("\nElegiste salir...\n")
+        os.system("pause")
+        break
+    else:
+        print("\nElija una opcion correcta...")
+    print()
+    os.system("pause")
+
+print()
